4607080b8588fbd970f2b031d0ecf007cbf0bd50
akram mohamed <cromi777@gmail.com>
2020-06-28 10:10:07 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'quote.dart';
+import 'quote_card.dart';
+
+void main() => runApp(MaterialApp(
+  home: QuoteList(),
+));
+
+class QuoteList extends StatefulWidget {
+  @override
+  _QuoteListState createState() => _QuoteListState();
+}
+
+class _QuoteListState extends State<QuoteList> {
+
+  List<Quote> quotes = [
+    Quote(text: 'What a Tribute! Listen to what Senator', author: 'sami omer'),
+    Quote(text: 'Claude Pepper  as he calls him.', author: 'ahmed ali'),
+    Quote(text: 'has to say about his won Teflon  Presiden,', author: 'mohaned osman'),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: Text('Awesome Quotes'),
+        backgroundColor: Colors.purpleAccent[400],
+        centerTitle: true,
+      ),
+      body: Column(
+        children: quotes.map((quote) => QuoteCard(
+            quote: quote,
+            delete: () {
+              setState(() {
+                quotes.remove(quote);
+              });
+            },
+        )).toList(),
+      ),
+    );
+  }
+}
